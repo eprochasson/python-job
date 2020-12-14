74742a4815383c071d086b3d65488addb264e59f
Emmanuel Prochasson <eprochasson@gmail.com>
2020-12-14 15:16:16 +0800

    Nicer

--- main.py
+++ main.py
@@ -9,21 +9,27 @@ if __name__ == '__main__':
     logger.info("Testing external dependency")
     try:
         import requests
+        logger.info("\tPassed!")
     except ModuleNotFoundError as e:
         logger.fatal("Dependency module not found")
 
-    logger.info("Loading external file")
-    f = open('data/test.csv').readlines()
-    logger.info("{} lines found in f".format(len(f)))
+    try:
+        logger.info("Loading external file")
+        f = open('data/test.csv').readlines()
+        logger.info("\t{} lines found in f".format(len(f)))
+    except FileNotFoundError as e:
+        logger.fatal("Missing external file!")
 
     logger.info("Loading local module")
     try:
         from lib.nothing import fn
         fn()
+        logging.info("\tPassed")
     except ModuleNotFoundError as e:
         logger.fatal("Local module not found")
 
 
+    logging.info("Scanning arguments...")
     arguments = sys.argv[1:]
     if len(arguments) == 0:
         logger.info("No arguments passed")
